5d1763a6ccbf7a17b51ae507f3ca8c9f599db72b
Shah Muhammad Jannatul Hasan <58700965+mdjannatulhasan@users.noreply.github.com>
2021-05-27 12:15:31 +0600

    1

--- js/main.js
+++ js/main.js
@@ -11,18 +11,29 @@ document.addEventListener('DOMContentLoaded', function() {
 
   $(document).ready(function(){
     $('.sidenav').sidenav();
-  });
-
-  $(document).ready(function(){
     $('ul li:not(:last)').click(function(){
       $('li').removeClass("active");
       $(this).addClass("active");
   });
-  });
-  $(document).ready(function(){
 	$('#burgerMenu').click(function(){
 		$('#sideNavbar').toggleClass('expanded');
 		$('#sideNavbar span').toggleClass('nav-link');
 		$('#mainBody').toggleClass('smaller');
 	});
+    /*~~~~~~~~~~~~~ Custom Dropdown Admin Info ~~~~~~~~~~~~~~~*/
+
+    jQuery('.dropdown-trigger').click(function(){
+        $(this).data('clicked', true);
+      });
+    $(document).on('click', function (event) {
+        if (jQuery('.dropdown-trigger').data('clicked')) {
+            $('#dropdown1').toggleClass('aaa');
+        }else if (!$(event.target).closest('#dropdown1').length) {
+            $('#dropdown1').removeClass('aaa');
+        }
+      });
+      $('.fsIcons span').click(function(){
+        $('span').removeClass("active");
+        $(this).addClass("active");
+    });
 });
